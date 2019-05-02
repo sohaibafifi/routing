b0c18ea058ae00bb033720ae80670f2e6eb3b7aa
AYaddaden <ea_yaddaden@esi.dz>
2019-05-02 11:52:21 +0200

    Added a general configuration header for different parameters

---
+++ src/Configurations.hpp
@@ -0,0 +1,19 @@
+//
+// Created by ali on 5/2/19.
+//
+
+#ifndef HYBRID_CONFIGURATIONS_HPP
+#define HYBRID_CONFIGURATIONS_HPP
+
+namespace Configuration{
+    const double precision = 10.0;
+
+    const int destruction_steps = 3;
+
+}
+
+
+
+
+
+#endif //HYBRID_CONFIGURATIONS_HPP
