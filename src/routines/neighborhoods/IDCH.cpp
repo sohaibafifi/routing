17261567db375a2f3bd6c5206342a80fb59bef70
AYaddaden <ea_yaddaden@esi.dz>
2019-05-02 12:00:38 +0200

    computes a big and small destruction and change max number of dmax to nbClient/nbTours

--- src/routines/neighborhoods/IDCH.cpp
+++ src/routines/neighborhoods/IDCH.cpp
@@ -1,17 +1,27 @@
 #include "IDCH.hpp"
+#include "../../Configurations.hpp"
+
 
 bool routing::IDCH::look(routing::models::Solution *solution)
 {
-    int itermax = solution->getProblem()->clients.size();
+    //itermax is equal to the square of number of clients
+    int itermax = solution->getProblem()->clients.size() * solution->getProblem()->clients.size();
     int iter = 1;
     routing::models::Solution *best = solution->clone();
     bool improved = false;
     double bestCost = solution->getCost();
-    unsigned dmax = solution->getProblem()->clients.size() / solution->getProblem()->vehicles.size();
+
+    //dmax takes two different values:
+    // for a big destruction: it takes nbClient / nbTours
+    //for a small destruction: it takes value of DESTRUCTION_STEPS
+    unsigned dmax = Configuration::destruction_steps;
 
     while (iter < itermax) {
         iter++;
+        dmax = ( (iter % solution->getProblem()->clients.size()  == 0)? solution->getProblem()->clients.size() / solution->getNbTour() :  Configuration::destruction_steps );
+
         destructor->destruct(solution, dmax);
+
         if(constructor->bestInsertion(solution) && solution->getCost() < bestCost - 1e-9)
         {
             bestCost = solution->getCost();
