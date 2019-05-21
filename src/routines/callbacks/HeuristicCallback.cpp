38f4e414a76ce913d875d2f681186c33fecd8635
AYaddaden <ea_yaddaden@esi.dz>
2019-05-21 12:04:32 +0200

    Modified the diver to take into consideration arc values between 0 and 1

--- src/routines/callbacks/HeuristicCallback.cpp
+++ src/routines/callbacks/HeuristicCallback.cpp
@@ -6,48 +6,84 @@
 #include "../../mtrand.hpp"
 #include "../../problems/cvrptw/routines/divers/Diver.hpp"
 
+#include <unistd.h>
+
 void routing::callback::HeuristicCallback::main()
 {
-    if (!hasIncumbent() ) {
-        getEnv().out() << "Construct solution from scratch " << std::flush;
-        if(! generator->generate(solution)){
-            //no solution generator is defined
-            std::cout <<  std::endl;
-            return ;
+
+
+    double randomValue = static_cast<double>(rand())/RAND_MAX;
+    //static int numberofDivings = 1;
+
+    /*
+
+        int nbVarToOne = 0;
+        for(int i = 0; i < problem->arcs.size(); i++ ){
+            for(int j = 0; j < problem->arcs.size(); j++ ){
+                if(std::abs(getValue(problem->arcs[i][j])-1) < Configuration::epsilon){
+                    nbVarToOne++;
+                }
+            }
         }
-        std::cout << solution->getCost()<< std::endl;
-    }
-    else {
-        if(neighbors.empty()) return;
-        extractSolution();
-        Utilities::MTRand_int32 irand(std::time(nullptr));
-        bool improved = false;
-        std::vector<bool> run(neighbors.size(), false);
-        while(std::find(run.begin(), run.end(), false) != run.end()){
-          unsigned i = 0;
-          do{i = irand() % run.size();} while(run[i]);
 
-             if(neighbors[i]->look(solution)){
-                 improved = true;
-                 run = std::vector<bool>(neighbors.size(), false);
-             }else {
-                 run[i] = true;
-             }
-        }
-        if(!improved) return;
-    }
+        float q = (nbVarToOne *100.0 / (problem->clients.size() + solution->getNbTour()))/100;
+        std::cout << "q is equal to " << q << std::endl;
+    */
 
 
-
-    if (!hasIncumbent() || solution->getCost() < getIncumbentObjValue() - 1e-9) {
+    if( randomValue <= 0.5 ){
         /*
          * Trying to dive
          */
-        solution = extractPartialSolution(problem);
-        routing::forbiddenPositions fp  = extractForbiddenPositions(problem);
-        CVRPTW::Diver* diver = new CVRPTW::Diver();
-        diver->dive(solution, &fp);
+       solution = extractPartialSolution(problem);
 
+         if(solution->toRoute.empty()){
+             generator->generate(solution);
+
+         }else{
+
+             routing::forbiddenPositions fp  = extractForbiddenPositions(problem);
+             CVRPTW::Diver* diver = new CVRPTW::Diver();
+             diver->dive(solution, &fp);
+
+         }
+    }
+    else{
+        if (!hasIncumbent() ) {
+            getEnv().out() << "Construct solution from scratch " << std::flush;
+            if(! generator->generate(solution)){
+                //no solution generator is defined
+                std::cout <<  std::endl;
+                return ;
+            }
+            std::cout << solution->getCost()<< std::endl;
+        }
+        else {
+            if(neighbors.empty()) return;
+            extractSolution();
+            Utilities::MTRand_int32 irand(std::time(nullptr));
+            bool improved = false;
+            std::vector<bool> run(neighbors.size(), false);
+            while(std::find(run.begin(), run.end(), false) != run.end()){
+                unsigned i = 0;
+                do{i = irand() % run.size();} while(run[i]);
+
+                if(neighbors[i]->look(solution)){
+                    improved = true;
+                    run = std::vector<bool>(neighbors.size(), false);
+                }else {
+                    run[i] = true;
+                }
+            }
+            if(!improved) return;
+        }
+
+    }
+
+    //numberofDivings++;
+
+
+    if (!hasIncumbent() || solution->getCost() < getIncumbentObjValue() - 1e-9) {
         IloNumVarArray vars(getEnv());
         IloNumArray vals(getEnv());
         solution->getVarsVals(vars, vals);
