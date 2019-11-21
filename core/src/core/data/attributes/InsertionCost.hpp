e45921a4ab1f72f493afb3d969a3a510b90e649b
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-11-21 22:32:00 +0100

    implement missed functions in vrp::tour

--- core/src/core/data/attributes/InsertionCost.hpp
+++ core/src/core/data/attributes/InsertionCost.hpp
@@ -10,7 +10,35 @@
 
 namespace routing {
     class InsertionCost {
+    private:
+        routing::Duration delta;
+        bool possible;
+    public:
+        bool operator>(const InsertionCost &rhs) const{
+            return delta > rhs.delta;
+        }
 
+
+        routing::Duration getDelta() const {
+            return delta;
+        }
+
+        void setDelta(routing::Duration delta){
+            InsertionCost::delta = delta;
+        }
+
+        bool isPossible() const{
+            return possible;
+        }
+
+        void setPossible(bool possible){
+            InsertionCost::possible = possible;
+        }
+
+        InsertionCost(routing::Duration delta, bool possible) : delta(delta), possible(possible) {}
+        InsertionCost() : delta(0), possible(true) {}
+
+        InsertionCost(const InsertionCost &cost): delta(cost.getDelta()), possible(cost.isPossible()){}
     };
 }
 
