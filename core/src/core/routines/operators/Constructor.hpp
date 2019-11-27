9863b5a35236f4b2d5d0154e88487925a9d2875b
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-11-27 19:35:23 +0100

    factorize constructor code

--- core/src/core/routines/operators/Constructor.hpp
+++ core/src/core/routines/operators/Constructor.hpp
@@ -6,13 +6,19 @@ namespace routing {
 
     class Constructor {
     public:
-        virtual bool bestInsertion(models::Solution *solution) = 0;
-        virtual bool bestInsertion(models::Solution *solution, models::Client *client) = 0;
+        virtual bool bestInsertion(models::Solution *solution){
+            return this->bestInsertion(solution, solution->notserved);
+        }
+        virtual bool bestInsertion(models::Solution *solution, models::Client *client) {
+            std::vector<models::Client*> clients = std::vector<models::Client*>();
+            clients.push_back(client);
+            return this->bestInsertion(solution, clients);
+        }
+        virtual bool bestInsertion(models::Solution *solution, std::vector<models::Client*> clients) = 0;
     };
 
     class dummyConstructor : public Constructor {
     public:
-        virtual bool bestInsertion(models::Solution *solution) { return false; }
-        virtual bool bestInsertion(models::Solution *solution, models::Client *client)  { return false; }
+        virtual bool bestInsertion(models::Solution *solution, std::vector<models::Client*> clients)  { return false; }
     };
 }
