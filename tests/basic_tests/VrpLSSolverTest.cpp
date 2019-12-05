82c08b12cfb8892b4a88f417cfa0403cbcb6dc1b
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2019-12-05 11:14:59 +0100

    null output when testing

--- tests/basic_tests/VrpLSSolverTest.cpp
+++ tests/basic_tests/VrpLSSolverTest.cpp
@@ -5,20 +5,24 @@
 #include <vrp/Reader.hpp>
 #include <vrp/Problem.hpp>
 #include <vrp/solvers/LSSolver.hpp>
+#include <fstream>
 #include "gtest/gtest.h"
 
 class VrpLSSolverTest : public ::testing::Test {
 public :
+    std::ofstream nullstream;
     VrpLSSolverTest() : Test() {
-
+    if (!nullstream.is_open())
+        nullstream.open("/dev/null", std::ofstream::out | std::ofstream::app);
     }
 };
 
 TEST_F(VrpLSSolverTest, solve){
-     vrp::LSSolver<vrp::Reader> lsSolver("");
-     lsSolver.solve();
-     EXPECT_EQ(lsSolver.getSolution()->getTour(1)->getNbClient(),
-             0);
-     EXPECT_GT(lsSolver.getSolution()->getCost(),
-             0);
+
+     vrp::LSSolver<vrp::Reader> lsSolver("", this->nullstream);
+     EXPECT_FALSE(dynamic_cast<vrp::Problem*>(lsSolver.getProblem())->clients.empty());
+     bool solution_found = lsSolver.solve();
+     EXPECT_TRUE(solution_found);
+     EXPECT_EQ(lsSolver.getSolution()->getTour(1)->getNbClient(), 0);
+     EXPECT_GT(lsSolver.getSolution()->getCost(), 0);
 }
