bbdbb24772febf61056b0a9b554570c3adec6ef7
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-02-18 09:58:53 +0100

    Add some comments

--- core/src/core/routines/neighborhoods/Neighborhood.hpp
+++ core/src/core/routines/neighborhoods/Neighborhood.hpp
@@ -5,6 +5,12 @@
 namespace routing {
     class Neighborhood {
     public :
+        /**
+         *
+         * @param solution: the solution to shake
+         *                  it returns the best neighbor
+         * @return true if solution improved
+         */
         virtual bool look(models::Solution *solution) = 0;
     };
 
--- tests/solver_tests/VrpLSSolverTest.cpp
+++ tests/solver_tests/VrpLSSolverTest.cpp
@@ -23,6 +23,7 @@ TEST_F(VrpLSSolverTest, solve){
      EXPECT_FALSE(dynamic_cast<vrp::Problem*>(lsSolver.getProblem())->clients.empty());
      bool solution_found = lsSolver.solve();
      EXPECT_TRUE(solution_found);
+     // there is no constraint so all the clients should be served within the first tour
      EXPECT_EQ(lsSolver.getSolution()->getTour(1)->getNbClient(), 0);
      EXPECT_GT(lsSolver.getSolution()->getCost(), 0);
 }
