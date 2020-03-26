e4dfa2a821a33d85d7d3db36527493ffc8229c37
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-03-26 11:34:33 +0100

    [cvrptw] add visits to tour
    Signed-off-by: Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>

--- problems/cvrptw/src/cvrptw/models/Tour.hpp
+++ problems/cvrptw/src/cvrptw/models/Tour.hpp
@@ -9,9 +9,43 @@
 
 namespace cvrptw {
     namespace models {
+        class InsertionCost : public routing::InsertionCost {
+        private:
+            routing::Duration start;
+            routing::Duration wait;
+        public:
+
+
+            routing::Duration getStart() const {
+                return start;
+            }
+
+            void setStart(routing::Duration start) {
+                InsertionCost::start = start;
+            }
+
+            routing::Duration getWait() const {
+                return wait;
+            }
+
+            void setWait(routing::Duration wait) {
+                InsertionCost::wait = wait;
+            }
+
+            InsertionCost(routing::Duration delta, routing::Duration start, routing::Duration wait, bool possible)
+                    : routing::InsertionCost(delta, possible), start(start), wait(wait) {}
+
+            InsertionCost() : routing::InsertionCost(), start(0), wait(0) {}
+
+            InsertionCost(const InsertionCost &cost)
+                    : routing::InsertionCost(cost), start(cost.getStart()), wait(cost.getWait()) {}
+
+        };
+
         class Tour : public vrp::models::Tour {
         protected:
             routing::Duration totalTime;
+            std::vector<Visit *> visits;
 
             Tour *clone() const override {
                 Tour *tour = new Tour(this->problem, this->getID());
@@ -21,14 +55,63 @@ namespace cvrptw {
 
         public:
             models::Client *getClient(unsigned long i) const override {
-                return dynamic_cast<Client *>(clients[i]);
+                return dynamic_cast<Client *>(visits[i]->client);
             }
 
             Tour(routing::Problem *p_problem, unsigned vehicleID) :
                     vrp::models::Tour(p_problem, vehicleID),
+                    visits(std::vector<Visit *>()),
                     totalTime(0) {}
 
             void update() override {}
+
+            void pushClient(routing::models::Client *client) override {
+                vrp::models::Tour::pushClient(client);
+                visits.push_back(new Visit(dynamic_cast<Client *>(client), 0, 0, 0));
+                this->update();
+            }
+
+            void addClient(routing::models::Client *client, unsigned long position) override {
+                //TODO : optimize to not recalculate the insertion cost
+                InsertionCost *insertion = static_cast<InsertionCost *>(this->evaluateInsertion(client, position));
+                traveltime += insertion->getDelta();
+                if (auto *consumer = dynamic_cast<routing::attributes::Consumer *>(client))
+                    consumption += consumer->getDemand();
+                clients.insert(clients.begin() + position, dynamic_cast<Client *>(client));
+
+                // vrp::models::Tour::addClient(client, position);
+                visits.insert(visits.begin() + position, new Visit(
+                        dynamic_cast<Client *>(client),
+                        insertion->getStart(),
+                        0,
+                        insertion->getWait()));
+                this->update();
+                updated = true;
+            }
+
+            void removeClient(unsigned long position) override {
+                vrp::models::Tour::removeClient(position);
+                visits.erase(visits.begin() + position);
+            }
+
+            void clear() override {
+                vrp::models::Tour::clear();
+                visits = std::vector<Visit *>();
+            }
+
+            unsigned long getNbClient() override {
+                return this->visits.size();
+            }
+
+
+            routing::InsertionCost *
+            evaluateInsertion(routing::models::Client *client, unsigned long position) override {
+                return vrp::models::Tour::evaluateInsertion(client, position);
+            }
+
+            routing::RemoveCost *evaluateRemove(unsigned long position) override {
+                return vrp::models::Tour::evaluateRemove(position);
+            }
 
         };
     }
